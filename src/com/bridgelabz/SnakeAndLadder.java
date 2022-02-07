47d1e484b1d150bf5eec8801c5426ba975981b32
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-07 21:02:16 +0530

    Added: checks for logic

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,8 +4,34 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.print("Welcome to Snake and Ladder Problem");
-		
+
+		int ladder = 1;
+		int snake = 2;
+		int movesAhead;
+		int movesBehind;
+
+		// single player at start position 0
 		int position = 0;
 		System.out.println("player position is = " + position);
+
+		// Player rolls the dice to get a number between 1 to 6
+		int value;
+		double rand = Math.random();
+		value = (int) (1 + rand * 6);
+		System.out.println("Dice number is = " + value);
+
+		// The Player then checks for a Option
+		int option;
+		option = (int) (rand * 3);
+		System.out.println("Option is = " + option);
+
+		if (option == ladder) {
+			movesAhead = (value + position);
+			System.out.println("Player moves ahead by the " + movesAhead);
+		} else if (option == snake) {
+			movesBehind = (value - position);
+			System.out.println("Player moves behind by the " + movesBehind);
+		} else
+			System.out.println("No Play.. the player stays in the same position");
 	}
 }
