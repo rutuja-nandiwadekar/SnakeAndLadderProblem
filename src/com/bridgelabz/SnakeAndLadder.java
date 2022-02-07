130a4443fa58d42d9f61f88f47640a6e478d295b
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-07 20:04:02 +0530

    Added: single player at start position logic

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,5 +4,14 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.print("Welcome to Snake and Ladder Problem");
+		
+		int position = 0;
+		System.out.println("player position is = " + position);
+		
+		int value;
+        double rand = Math.random();
+        value = (int) (1 + rand * 6);
+
+        System.out.println("Dice number is = " + value);
 	}
 }
